9d41d58eadabe146cf11fea8da1126051fc731ba
mahmoudzenbarakji <mahmmoud.zenbarakji@gmail.com>
2026-01-13 16:33:49 +0300

    c

--- Backend/server.js
+++ Backend/server.js
@@ -176,9 +176,7 @@ app.use((req, res) => {
         dbConnected = false;
     }
 })();
-app.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000');
-});
+
 // ==============================
 // Export app for Vercel serverless
 // ==============================
